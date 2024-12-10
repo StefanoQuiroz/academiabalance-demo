40936df2199d16aaaa5b0c829694dac0af42bd7f
toy-story-car <piero.quiroz93@gmail.com>
2024-12-09 21:26:16 -0500

    feat: add metadata

--- app/page.js
+++ app/page.js
@@ -1,41 +1,41 @@
-import AboutSection from '@/component/about/AboutSection';
-import ActivitySection from '@/component/activity/ActivitySection';
-import BannerSection from '@/component/banner/BannerSection';
-import BlogSection from '@/component/blog/BlogSection';
-import CategorySection from '@/component/category/CategorySection';
-import EventSection from '@/component/event/EventSection';
-import FaqSection from '@/component/faq/FaqSection';
-import FooterSection from '@/component/footer/FooterSection';
-import VideoModal from '@/component/modal/VideoModal';
-import NavbarSection from '@/component/navbar/NavbarSection';
-import TestimonialSection from '@/component/testimonial/TestimonialSection';
-import ScrollToTopButton from '@/component/utils/ScrollToTopButton';
-import VideoSection from '@/component/video/VideoSection';
-import WorkSection from '@/component/work/WorkSection';
+import AboutSection from "@/component/about/AboutSection";
+import ActivitySection from "@/component/activity/ActivitySection";
+import BannerSection from "@/component/banner/BannerSection";
+import BlogSection from "@/component/blog/BlogSection";
+import CategorySection from "@/component/category/CategorySection";
+import EventSection from "@/component/event/EventSection";
+import FaqSection from "@/component/faq/FaqSection";
+import FooterSection from "@/component/footer/FooterSection";
+import VideoModal from "@/component/modal/VideoModal";
+import NavbarSection from "@/component/navbar/NavbarSection";
+import TestimonialSection from "@/component/testimonial/TestimonialSection";
+import ScrollToTopButton from "@/component/utils/ScrollToTopButton";
+import VideoSection from "@/component/video/VideoSection";
+import WorkSection from "@/component/work/WorkSection";
 
 export const metadata = {
-    title: 'Eduor Home Page 1',
-    description: 'Developed by Azizur Rahman',
+  title: "Academia Balance Demo",
+  description: "Stefano Quiroz - Nebulabs",
 };
 
 export default function Home() {
-    return (
-        <>
-            <NavbarSection style='' logo='images/logo-academia.svg' />
-            <BannerSection />
-            {/* <CategorySection /> */}
-            {/* <AboutSection /> */}
-            <EventSection section='tf__event mt_95' startIndex={0} endIndex={4} />
-            {/* <FaqSection img='images/faq_person.png' /> */}
-            <WorkSection />
-            <TestimonialSection />
-            {/* <ActivitySection /> */}
-            <FaqSection img='images/faq_person.png' />
-            {/* <VideoSection /> */}
-            {/* <BlogSection /> */}
-            <FooterSection />
-            {/* <VideoModal /> */}
-            <ScrollToTopButton style='' />
-        </>
-    );
+  return (
+    <>
+      <NavbarSection style="" logo="images/logo-academia.svg" />
+      <BannerSection />
+      {/* <CategorySection /> */}
+      {/* <AboutSection /> */}
+      <EventSection section="tf__event mt_95" startIndex={0} endIndex={4} />
+      {/* <FaqSection img='images/faq_person.png' /> */}
+      <WorkSection />
+      <TestimonialSection />
+      {/* <ActivitySection /> */}
+      <FaqSection img="images/faq_person.png" />
+      {/* <VideoSection /> */}
+      {/* <BlogSection /> */}
+      <FooterSection />
+      {/* <VideoModal /> */}
+      <ScrollToTopButton style="" />
+    </>
+  );
 }
